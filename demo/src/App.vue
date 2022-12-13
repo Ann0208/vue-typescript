960a60a185f942ab26aed8977a50b894749dd547
yanhui <chenjingyi01@bilibili.com>
2022-12-13 17:30:21 +0800

    init

---
+++ demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <p>....</p>
+    <img alt="Vue logo" src="./assets/cat.png">
+    <HelloWorld msg="What are you looking at? It's just a kitten" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>vue - typescript</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  }
+});
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
